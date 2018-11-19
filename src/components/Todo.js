aa731894e5c200560b5cae512bb1d0bcb32e4c46
Stan Chen <solve153@gmail.com>
2018-11-19 23:39:21 +0800

    todo list day and night display different color

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const TodoText = styled.div`
+  color: ${(props) => {
+    const now = new Date();
+    const hours = now.getHours();
+    const color = hours >= 6 && hours <= 18 ? 'black' : 'green';
+    return color;
+  }};
+`;
 
 class Todo extends Component {
   render() {
-    return <h3>{this.props.todo}</h3>
+    return <TodoText>{this.props.todo}</TodoText>
   }
 }
 
